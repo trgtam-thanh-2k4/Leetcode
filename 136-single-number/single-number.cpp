4d70f6942c8430bd21557dd04796a9665fa00cb0
TRUONG TAM THANH <113973088+trgtam-thanh-2k4@users.noreply.github.com>
2023-11-26 14:03:37 +0700

    Time: 23 ms (21.07%) | Memory: 17.3 MB (31.47%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        ranges::sort(nums);
+        int count = 1;
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] == nums[i - 1])
+            {
+                count++;
+            }
+            else 
+            {
+                if (count == 1) return nums[i - 1];
+                count = 1;
+            }
+        }
+        return nums[nums.size() - 1];
+    }
+};
