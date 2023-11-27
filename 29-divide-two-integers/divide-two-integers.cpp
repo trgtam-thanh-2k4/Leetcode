1aa78a75a44619784506920a1b5aa778e2e41916
TRUONG TAM THANH <113973088+trgtam-thanh-2k4@users.noreply.github.com>
2023-11-27 14:31:35 +0700

    Time: 5 ms (28.43%) | Memory: 6.3 MB (71.21%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1)
+            return INT_MAX;
         long long result = (long long)dividend / (long long)divisor;
 
-        return (int)(result > INT32_MAX ? INT32_MAX : result < INT32_MIN ? INT32_MIN : result);
+        //return (int)(result > INT32_MAX ? INT32_MAX : result < INT32_MIN ? INT32_MIN : result);
+        return (int)result;
     }
 };
