22ac7c378ba508acabeb01ef8327a305c45f44f0
TRUONG TAM THANH <113973088+trgtam-thanh-2k4@users.noreply.github.com>
2023-12-01 00:03:18 +0700

    Time: 148 ms (15.62%) | Memory: 61.6 MB (57.60%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,14 +1,5 @@
 class Solution {
 public:
-    int findMax(vector<int> nums)
-    {
-        int max = nums[0];
-        for (int i = 1; i < nums.size(); i++)
-        {
-            if (nums[i] > max) max = nums[i];
-        }
-        return max;
-    }
     int findDuplicate(vector<int>& nums) {
         ranges::sort(nums);
         for (int i = 1; i < nums.size(); i++)
