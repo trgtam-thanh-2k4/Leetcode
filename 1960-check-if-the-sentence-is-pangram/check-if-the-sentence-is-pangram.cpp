dc5d62cc9ca2422e470ce066355b071591d4c483
TRUONG TAM THANH <113973088+trgtam-thanh-2k4@users.noreply.github.com>
2023-12-13 18:23:10 +0700

    Time: 0 ms (100.00%) | Memory: 6.8 MB (92.82%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        vector<int> ans(26, 0);
+        for (int i = 0; i < sentence.length(); i++)
+        {
+            ans[sentence[i] - 'a']++;
+        }
+        for (int i = 0; i < 26; i++)
+        {
+            if (ans[i] == 0) return false;
+        }
+        return true;
+    }
+};
